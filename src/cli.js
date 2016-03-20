ebaed086c169a242c1f64f6ed60c69284caf8871
Thiago Felix <thiago@thiagofelix.com>
2016-03-20 13:06:24 -0300

    fix(cli): Adiciona funcionalidade de executar consultas de nfe pela linha de comando

---
+++ src/cli.js
@@ -0,0 +1,18 @@
+import { consultar } from './nfce'
+
+const url = process.argv.pop()
+
+if(!/\d{44}/.test(url)) {
+  throw new Error("URL Inválida");
+}
+
+consultar(url)
+  .catch(err => {
+    throw new Error(err)
+  })
+  .then(nota => {
+    console.log(JSON.stringify(nota, null, 4))
+  })
+
+
+
