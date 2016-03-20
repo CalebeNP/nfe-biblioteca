6df258f955f079f1dadb919f46012c868e6ba66a
Thiago Felix <thiago@thiagofelix.com>
2016-03-20 12:34:31 -0300

    test(sefaz-parana): Adiciona testes do sefaz-para a lista de ignorados pelo travis pois o sistema se

--- test/parana.test.js
+++ test/parana.test.js
@@ -1,7 +1,7 @@
 import {expect} from 'chai'
 import {consultar} from '../src/secretarias/parana'
 
-describe('NFCe Parana', function () {
+describe('NFCe Parana @noci', function () {
   describe('.consultar', function () {
     vcr(it('converte NFCe para JSON', async () => {
       const link = 'http://www.dfeportal.fazenda.pr.gov.br/dfe-portal/rest/servico/consultaNFCe?chNFe=41160214953750000192650020000777871331742869&nVersao=100&tpAmb=1&dhEmi=323031362d30322d32315430373a33383a35302d30333a3030&vNF=135.72&vICMS=0.00&digVal=36397577774536594c6c5233547a774a4c7431764273496634636b3d&cIdToken=000001&cHashQRCode=940323cec1085f246de3f87b7b069556cd58aae7'
